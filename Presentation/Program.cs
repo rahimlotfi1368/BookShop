3212c5de489f071c00915194acb96569b2f87c88
rahimlotfi1368 <sinuhe_1368@yahoo.com>
2024-08-14 00:53:05 +0330

    init Commit

---
+++ Presentation/Program.cs
@@ -0,0 +1,14 @@
+namespace Presentation;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Hello World!");
+
+        app.Run();
+    }
+}
